72d20003abc0e32b683f0e7f2c0452738515bda2
thaislsilveira <thais_lsilveira>
2020-08-12 20:55:43 -0300

    Configurando a navegação

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import Routes from './routes';
 
 import Header from './components/Header';
-import Main from './pages/main';
 
 import './styles.css';
 
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
   </div>
 );
 
