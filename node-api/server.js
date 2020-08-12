7ca60a61b61ed4b96e9240a6769c2a1339149475
thaislsilveira <thais_lsilveira>
2020-08-11 21:32:06 -0300

    criando model de produto

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
@@ -7,8 +8,17 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useNewUrlParser: true,
 });
 
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
-  res.send('Hello Rocketseat');
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native',
+  });
+  return res.send('Hello Rocketseat');
 });
 
 app.listen(3001);
