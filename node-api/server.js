63a8707f8a0e71219225d7741bea6ed223aa006a
thaislsilveira <thais_lsilveira>
2020-08-11 21:21:41 -0300

    conectando Database

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello Rocketseat');
+});
 
 app.listen(3001);
