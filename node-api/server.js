c2692c549ff6c9cd8f9bd8b9d3c4a311d2b7695e
thaislsilveira <thais_lsilveira>
2020-08-11 21:43:17 -0300

    Reestruturação de arquivos

--- node-api/server.js
+++ node-api/server.js
@@ -10,15 +10,6 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get('/', (req, res) => {
-  Product.create({
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native',
-  });
-  return res.send('Hello Rocketseat');
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
---
+++ node-api/src/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const ProductController = require('./controller/ProductController');
+
+routes.get('/products', ProductController.index);
+
+module.exports = routes;
